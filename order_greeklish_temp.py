0e1ce295d284709b1e68c8b20c51d524dbdb3f02
George Pamfilis <gpamfilis@gmail.com>
2020-06-25 11:55:15 +0300

    added a configuration json

--- order_greeklish_temp.py
+++ order_greeklish_temp.py
@@ -151,54 +151,63 @@ def list_checkouts(store_id=1):
 
 
 # order_setup(orders[0])
+try:
 
-while True:
-    try:
-        order_ids = list_orders()[:]
-        # checkout_ids = list_checkouts()[:]
-        print('orders: ', order_ids)
-        # print('checkouts: ', checkout_ids)
-        time.sleep(5)
+    with open('~/Desktop/config.json') as f:
+    data = json.load(f)
+    store_id = data['store_id']
 
-        for order_id in order_ids:
+
+    while True:
+        try:
             order_ids = list_orders()[:]
-            # print(order_ids)
-            print(order_id)
-            time.sleep(1)
-            try:
-                order_setup(order_id)
-                res_pos = requests.post('http://www.e-orders.org/api/printer/orders-print?order_id={0}'.format(order_id))
-                print('responce', res_pos)
-            except Exception as e:
-                print(e)
-                print('deleting')
-                res_delete = requests.delete('http://www.e-orders.org/api/v1/mobile/printerorders?order_id={0}'.format(order_id))
+            # checkout_ids = list_checkouts()[:]
+            print('orders: ', order_ids)
+            # print('checkouts: ', checkout_ids)
+            time.sleep(5)
 
-        # for checkout_id in checkout_ids:
-        #     checkout_ids = list_checkouts()[:]
-        #     # print(order_ids)
-        #     print(checkout_id)
-        #     time.sleep(1)
-        #     try:
-        #         checkout_setup(checkout_id)
-        #         res_pos = requests.post('http://www.e-orders.org/api/printer/checkout-print?checkout_id={0}'.format(checkout_id))
-        #         print('responce', res_pos)
-        #     except Exception as e:
-        #         print(e)
-    except Exception as e:
-        print(e)
+            for order_id in order_ids:
+                order_ids = list_orders(store_id=store_id)[:]
+                # print(order_ids)
+                print(order_id)
+                time.sleep(1)
+                try:
+                    order_setup(order_id)
+                    res_pos = requests.post('http://www.e-orders.org/api/printer/orders-print?order_id={0}'.format(order_id))
+                    print('responce', res_pos)
+                except Exception as e:
+                    print(e)
+                    print('deleting')
+                    res_delete = requests.delete('http://www.e-orders.org/api/v1/mobile/printerorders?order_id={0}'.format(order_id))
 
-# # p = Usb(0x471, 0x55, 0, 0x82, 0x2)
-# # p.device.write(p.out_ep, RT_STATUS_PAPER, 5000)
-# # p.device.read(p.in_ep, 256, 5000)
-# # p.close()
-#
-# import usb
-# # dev = None
-# # while dev is None:
-# dev = usb.core.find(idVendor=0x471, idProduct=0x55)
-# #
-# msg = 'text'
-# dev.write(0x2, RT_STATUS_PAPER, 10000)
-# ret = dev.read(0x82, len(RT_STATUS_PAPER), 5000)
-# sret = ''.join([chr(x) for x in ret])
+            # for checkout_id in checkout_ids:
+            #     checkout_ids = list_checkouts()[:]
+            #     # print(order_ids)
+            #     print(checkout_id)
+            #     time.sleep(1)
+            #     try:
+            #         checkout_setup(checkout_id)
+            #         res_pos = requests.post('http://www.e-orders.org/api/printer/checkout-print?checkout_id={0}'.format(checkout_id))
+            #         print('responce', res_pos)
+            #     except Exception as e:
+            #         print(e)
+        except Exception as e:
+            print(e)
+
+    # # p = Usb(0x471, 0x55, 0, 0x82, 0x2)
+    # # p.device.write(p.out_ep, RT_STATUS_PAPER, 5000)
+    # # p.device.read(p.in_ep, 256, 5000)
+    # # p.close()
+    #
+    # import usb
+    # # dev = None
+    # # while dev is None:
+    # dev = usb.core.find(idVendor=0x471, idProduct=0x55)
+    # #
+    # msg = 'text'
+    # dev.write(0x2, RT_STATUS_PAPER, 10000)
+    # ret = dev.read(0x82, len(RT_STATUS_PAPER), 5000)
+    # sret = ''.join([chr(x) for x in ret])
+
+except Exception as e:
+    print(e)
